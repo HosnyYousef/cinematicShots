caba31ef4f80af6420a0891a8e2d1d833418fd0b
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-04-05 14:27:35 +0200

    main.js notes

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,11 @@ async function apiRequest() {
         const data = await response.json()
 
         console.log(data)
-        // was:  document.querySelector('h2').innerText = data.suggestions
+    // How to display API as a list in HTML?
+        // was(before):  document.querySelector('h2').innerText = data.suggestions
         let listItems = data.suggestions.map(s => `<li>${s}</li>`).join('  ');
         document.querySelector('h2').innerHTML = `<ul>${listItems}</ul>`;
+        
     } catch (error) {
         console.log(error)
     }

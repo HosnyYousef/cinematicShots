3be4ec7abfdd3199fe7d75552346e85ccc9755ee
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-04-05 14:10:53 +0200

    display list as html

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,17 @@ alert('Working')
 
 document.querySelector('button').addEventListener('click', apiRequest)
 
-async function apiRequest(){
+async function apiRequest() {
     const cameraShots = document.querySelector('input').value
-    try{
+    try {
         const response = await fetch(`https://simple-cinematicshots-c0b820fcfd57.herokuapp.com/api/${cameraShots}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.suggestions
-    }catch(error){
+        // was:  document.querySelector('h2').innerText = data.suggestions
+        let listItems = data.suggestions.map(s => `<li>${s}</li>`).join('  ');
+        document.querySelector('h2').innerHTML = `<ul>${listItems}</ul>`;
+    } catch (error) {
         console.log(error)
     }
 }
